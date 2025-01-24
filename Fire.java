c1e712f8d21507829a3c83be6f73aad4379f3e46
vashistarai <vasshistarai@gmail.com>
2025-01-24 22:56:41 +0530

    ass trial

---
+++ Fire.java
@@ -0,0 +1,33 @@
+package com.aa.ass1;
+
+public class Fire {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+		System.out.println("e");
+	}
+
+}
